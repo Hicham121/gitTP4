3a3f49935c6fc4d6a860b45ab90a68bf4a8db42a
hicham <elaroussihicham@gmail.com>
2024-05-18 11:44:48 +0100

    Add read method in my application JSON.

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import applicationJSON as app
 
-# Afficher la liste des notes
+# Print the list of student notes by detail
 for eleve in app.liste_notes:
     print("Nom:" + eleve["nom"])
     print("Prenom:" +eleve["prenom"])
     print("Note:" +str(eleve["note"]))
+
+# Print the list of student notes by object
+for eleve in app.liste_notes:
+    print(eleve)
+
+# Print the list of student notes by list
+print(app.liste_notes)
